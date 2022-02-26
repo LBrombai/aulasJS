33f9432af35f27c8c9b91dffb29b3402d8f69e6d
Lucas <lucasbrombai@gmail.com>
2022-02-26 17:52:58 -0300

    initial commit

---
+++ funcoes.js
@@ -0,0 +1,8 @@
+// São tipos de objetos com a incrivel capacidade de serem invocados
+
+//Podem ser literais
+function myFunc(){
+    //teste
+}
+
+//hoisting é o ato de acionar uma função/variavel antes da linha da mesma
